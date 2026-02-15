a2faad187bb50059b4061d7ca96c282a3f8befae
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-02-15 23:25:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "google_compute_network" "vpc_network" {
 
 # Create a subnetwork within the custom VPC
 resource "google_compute_subnetwork" "private-subnet" {
+  project       = var.project
   name          = "terraform-subnet-us-central1"
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
